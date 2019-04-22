91839617f9f62c6832646ee0fc9c0838eb987486
Erick Daniszewski <edaniszewski@gmail.com>
2019-04-22 07:52:51 -0400

    update dependencies for CVE-2019-11324

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'iso8601',
-        'requests>=2.20.0',
+        'requests>=2.21.0',
     ],
     classifiers=[
         'Intended Audience :: Developers',
