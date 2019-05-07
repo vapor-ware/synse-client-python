ba189dfcab2ae9466f6513ecb3491fc4a5d9da48
Erick Daniszewski <edaniszewski@gmail.com>
2019-05-07 10:32:08 -0400

    Update client for Synse v3 API spec (#9)

--- synse/__init__.py
+++ synse/__init__.py
@@ -1,9 +1,9 @@
 """synse - a Python client for interacting with Synse Server."""
 
 __title__ = 'synse'
-__version__ = '0.0.2'
-__description__ = 'A python HTTP client for Synse Server.'
+__version__ = '3.0.0'
+__description__ = 'The official Python client for interacting with the Synse Server API.'
 __author__ = 'Vapor IO'
-__author_email__ = 'vapor@vapor.io'
-__url__ = 'https://github.com/vapor-ware/***'
+__author_email__ = 'erick@vapor.io'
+__url__ = 'https://github.com/vapor-ware/synse-client-python'
 __license__ = 'GNU General Public License v3.0'
