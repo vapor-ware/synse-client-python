ab92dfea877f901042b5c350b0f7ead2955d849a
Erick Daniszewski <edaniszewski@gmail.com>
2019-05-14 12:59:41 -0400

    add websocket support to the client (#12)

--- synse/client.py
+++ synse/client.py
@@ -1,11 +1,14 @@
 """API clients for Synse Server."""
 
+import asyncio
+import itertools
 import json
 import logging
 from typing import Generator, Iterable, List, Union
 
 import requests
 import requests.exceptions
+import websockets
 
 from synse import errors, models
 
@@ -73,7 +76,7 @@ class HTTPClientV3:
                 **kwargs,
             )
         except requests.exceptions.RequestException as e:
-            log.error(f'failed to issue request {method.upper()} {url}: {e}')
+            log.error(f'failed to issue request {method.upper()} {url} -> {e}')
             raise errors.SynseError from e
 
         errors.wrap_and_raise_for_error(response)
@@ -394,10 +397,341 @@ class HTTPClientV3:
         return [models.TransactionStatus(response, raw) for raw in response.json()]
 
 
+class WSSession:
+    """A class which models a WebSocket 'session'.
+
+    For the Synse API, each WebSocket message is sent with an ID which allows it
+    to match requests to responses. Each session is in charge of managing the
+    request IDs for sent events. By default, each client will have its own session.
+    """
+
+    def __init__(self, host, port, api_version):
+        self.host = host
+        self.port = port
+        self.api_version = api_version
+
+        self.connect_url = f'ws://{host}:{port}/{api_version}/connect'
+        self._id = itertools.count()
+        self._lock = asyncio.Lock()
+
+    async def _next(self):
+        async with self._lock:
+            return next(self._id)
+
+    async def request(self, event, data=None):
+        try:
+            async with websockets.connect(self.connect_url) as ws:
+                message = {
+                    'id': await self._next(),
+                    'event': event,
+                }
+                if data:
+                    message['data'] = data
+
+                await ws.send(json.dumps(message))
+
+                response = await ws.recv()
+                print(f'response ({type(response)}): {response}')
+                return json.loads(response)
+        except ConnectionError as e:
+            log.error(f'failed to issue request {event} {self.host}:{self.port} -> {e}')
+            raise errors.SynseError from e
+
+
 class WebsocketClientV3:
-    """"""
+    """A WebSocket client for Synse Server's v3 API.
+
+    Args:
+        host (str): The hostname/IP address for the Synse Server instance.
+        port (int): The port that Synse Server is listening on. (default: 5000)
+        session ():
+    """
 
     api_version = 'v3'
 
+    def __init__(self, host, port=5000, session=None):
+        self.host = host
+        self.port = port
+        self.session = session or WSSession(host, port, self.api_version)
+
     def __str__(self):
-        return f'<Synse WebSocket Client ({self.api_version}): >'
+        return f'<Synse WebSocket Client ({self.api_version}): {self.host}:{self.port}>'
+
+    def __repr__(self):
+        return self.__str__()
+
+    async def config(self) -> models.Config:
+        """Get the unified configuration for the Synse Server instance.
+
+        Returns:
+            The Synse v3 API config response.
+        """
+
+        r = await self.session.request('request/config')
+        return models.Config(response=r, raw=r['data'])
+
+    async def info(self, device: str) -> models.DeviceInfo:
+        """Get all information associated with the specified device.
+
+        Args:
+            device (str): The ID of the device to get information for.
+
+        Returns:
+            The Synse v3 API info response.
+        """
+
+        r = await self.session.request('request/info', data={
+            'device': device,
+        })
+        return models.DeviceInfo(response=r, raw=r['data'])
+
+    async def plugin(self, plugin_id: str) -> models.PluginInfo:
+        """Get all information associated with the specified plugin.
+
+        Args:
+            plugin_id (str): The ID of the plugin to get information for.
+
+        Returns:
+            The Synse v3 API plugin response.
+        """
+
+        r = await self.session.request('request/plugin', data={
+            'plugin': plugin_id,
+        })
+        return models.PluginInfo(response=r, raw=r['data'])
+
+    async def plugin_health(self) -> models.PluginHealth:
+        """Get a summary of the health of all currently registered plugins.
+
+        Returns:
+            The Synse v3 API plugin health response.
+        """
+
+        r = await self.session.request('request/plugin_health')
+        return models.PluginHealth(response=r, raw=r['data'])
+
+    async def plugins(self) -> Iterable[models.PluginSummary]:
+        """Get a summary of all plugins currently registered with the Synse Server instance.
+
+        Returns:
+            The Synse v3 API plugins response.
+        """
+
+        r = await self.session.request('request/plugin', data={})
+        return [models.PluginSummary(response=r, raw=raw) for raw in r['data']]
+
+    async def read(self, ns: str = None, tags: List[str] = None) -> Iterable[models.Reading]:
+        """Get the latest reading(s) for all devices which match the specified selector(s).
+
+        Args:
+            ns (str): The default namespace to use for the tags which do not
+                include a namespace. (default: default)
+            tags (list[str]): The tags to filter devices on.
+
+        Returns:
+            The Synse v3 API read response.
+        """
+
+        data = {
+            'ns': ns,
+            'tags': tags,
+        }
+        data = {k: v for k, v in data.items() if v is not None}
+
+        r = await self.session.request('request/read', data=data)
+        return [models.Reading(response=r, raw=raw) for raw in r['data']]
+
+    async def read_cache(self, start: str = None, end: str = None) -> Generator[models.Reading, None, None]:
+        """Get a window of cached device readings.
+
+        Args:
+            start (str): An RFC3339 formatted timestamp which specifies a starting
+                bound on the cache data to return. If no timestamp is specified,
+                there will not be a starting bound.
+            end (str): An RFC3339 formatted timestamp which specifies an ending
+                bound on the cache data to return. If no timestamp is specified,
+                there will not be an ending bound.
+
+        Yields:
+            The Synse v3 API read cache response.
+        """
+
+        data = {
+            'start': start,
+            'end': end,
+        }
+        data = {k: v for k, v in data.items() if v is not None}
+
+        r = await self.session.request('request/read_cache', data=data)
+        for raw in r['data']:
+            yield models.Reading(response=r, raw=raw)
+
+    async def read_device(self, device: str) -> Iterable[models.Reading]:
+        """Get the latest reading(s) for the specified device.
+
+        Args:
+            device (str): The ID of the device to get readings for.
+
+        Returns:
+            The Synse v3 API read device response.
+        """
+
+        r = await self.session.request('request/read_device', data={
+            'device': device,
+        })
+        return [models.Reading(response=r, raw=raw) for raw in r['data']]
+
+    async def scan(
+            self, force: bool = None, ns: str = None, sort: str = None, tags: List[str] = None,
+    ) -> Generator[models.DeviceSummary, None, None]:
+        """Get a summary of all devices currently exposed by the Synse Server instance.
+
+        Args:
+            force (bool): Force a re-scan (do not use the cache). If True, the
+                request will take longer since the internal device cache will
+                be rebuilt. Forcing a scan will ensure the set of returned devices
+                is up-to-date.
+            ns (str): The default namespace to use for the tags which do not
+                include a namespace. (default: default)
+            sort (str): Specify the fields to sort by. Multiple fields may be
+                specified as a comma-separated string, e.g. "plugin,id". The
+                "tags" field can not be used for sorting. (default:
+                "plugin,sort_index,id", where the sort_index is an internal sort
+                preference which a plugin can optionally specify.)
+            tags (list[str]): The tags to filter devices on.
+
+        Returns:
+            The Synse v3 API scan response.
+        """
+
+        data = {
+            'force': force,
+            'ns': ns,
+            'sort': sort,
+            'tags': tags,
+        }
+        data = {k: v for k, v in data.items() if v is not None}
+
+        r = await self.session.request('request/scan', data=data)
+        return (models.DeviceSummary(response=r, raw=raw) for raw in r['data'])
+
+    async def status(self) -> models.Status:
+        """Check the availability and connectivity status of the Synse Server instance.
+
+        If the instance is reachable, this request will resolve; otherwise, it will
+        raise an error.
+
+        Returns:
+            The Synse v3 API status response.
+        """
+
+        r = await self.session.request('request/status')
+        return models.Status(response=r, raw=r['data'])
+
+    async def tags(self, ns: str = None, ids: bool = None) -> Iterable[str]:
+        """Get a list of the tags currently associated with registered devices.
+
+        Args:
+            ns (str): The tag namespace(s) to use when searching for tags.
+                (default: default)
+            ids (bool): Include ID tags in the response. (default: false)
+
+        Returns:
+            The Synse v3 API tags response.
+        """
+
+        data = {
+            'ns': ns,
+            'ids': ids,
+        }
+        data = {k: v for k, v in data.items() if v is not None}
+
+        r = await self.session.request('request/tags', data=data)
+        return r['data']
+
+    async def transaction(self, transaction_id: str) -> models.TransactionStatus:
+        """Get the status of an asynchronous write transaction.
+
+        Args:
+            transaction_id (str): The ID of the transaction to get the status of.
+
+        Returns:
+            The Synse v3 API transaction response.
+        """
+
+        r = await self.session.request('request/transaction', data={
+            'transaction': transaction_id,
+        })
+        return models.TransactionStatus(response=r, raw=r['data'])
+
+    async def transactions(self) -> Iterable[str]:
+        """Get a list of the transactions currently tracked by Synse.
+
+        Returns:
+            The Synse v3 API transactions response.
+        """
+
+        r = await self.session.request('request/transaction')
+        return r['data']
+
+    async def version(self) -> models.Version:
+        """Get the version information for the configured Synse Server instance.
+
+        Returns:
+            The Synse v3 API version response.
+        """
+
+        r = await self.session.request('request/version')
+        return models.Version(response=r, raw=r['data'])
+
+    async def write_async(
+            self, device: str, payload: Union[List[dict], dict],
+    ) -> Iterable[models.TransactionInfo]:
+        """Write to the specified device asynchronously.
+
+        This method will queue up a write with the device's plugin and will
+        immediately return information for the transaction associated with
+        the write. This transaction can be checked later to ensure the write
+        completed successfully.
+
+        Args:
+            device (str): The ID of the device to write to.
+            payload (list[dict] | dict): The write payload.
+
+        Returns:
+            The Synse v3 API asynchronous write response.
+        """
+
+        data = {
+            'device': device,
+            'payload': payload,
+        }
+        data = {k: v for k, v in data.items() if v is not None}
+
+        r = await self.session.request('request/write_async', data=data)
+        return [models.TransactionInfo(response=r, raw=raw) for raw in r['data']]
+
+    async def write_sync(
+            self, device: str, payload: Union[List[dict], dict],
+    ) -> Iterable[models.TransactionStatus]:
+        """Write to the specified device synchronously.
+
+        This method will wait until the write action has completed. It is up
+        to the caller to ensure that a suitable timeout is set for the request.
+
+        Args:
+            device (str): The ID of the device to write to.
+            payload (list[dict] | dict): The write payload.
+
+        Returns:
+            The Synse v3 API synchronous write response.
+        """
+
+        data = {
+            'device': device,
+            'payload': payload,
+        }
+        data = {k: v for k, v in data.items() if v is not None}
+
+        r = await self.session.request('request/write_sync', data=data)
+        return [models.TransactionStatus(response=r, raw=raw) for raw in r['data']]
